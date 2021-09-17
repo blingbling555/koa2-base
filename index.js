095048d7d91a0a451c6d806ebd116aa7d654c967
abby <2016644182@qq.com>
2021-09-17 19:55:17 +0800

    增加koa-json-error来捕获错误 以及判断环境，在生产环境不要抛出堆栈信息

--- index.js
+++ index.js
@@ -2,6 +2,24 @@ const Koa = require('koa')
 const app = new Koa()
 const BodyParser = require('koa-bodyparser')
 const routing = require('./routes');
+const error = require('koa-json-error')
+// 自己写的错误处理
+// app.use(async (ctx, next) => {
+//   try {
+//     await next()
+//   } catch (err) {
+//     ctx.status = err.status || err.statusCode || 500
+//     ctx.body = {
+//       message: err.message
+//     }
+//   }
+// })
+
+// 用koa-json-error 404或者其他自己写的错误以及运行时错误都可以捕获，生产环境不要抛出堆栈信息
+app.use(error({
+  postFormat: (e, { stack, ...rest }) => process.env.NODE_ENV === 'production' ? rest : { stack, ...rest }
+}))
+
 app.use(BodyParser())
 routing(app)
 
